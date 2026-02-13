22c86b092a5614a2f2458e79c1c4c99ab37be361
Mix that maName <natakongame@gmail.com>
2026-02-13 21:42:03 +0700

    Delete QRcode.py

--- QRcode.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-filename = input("Enter filename to save QR code: ")
-urlQRcode = input("Enter URL to generate QR code: ")
-
-QRcode = qrcode.make(urlQRcode)
-QRcode.save(f"D:/MyGit/MyValentiny/{filename}.png")
-
-print(f"QR code generated and saved as 'MyValentiny/{filename}.png'")
